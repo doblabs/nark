6f6356c811a22241a065ab81bf20b7bb38aef1c9
Landon Bouma <landonb@users.noreply.github.com>
2023-12-18 15:37:47 -0600

    Kluge: Disable `isort` before any comment atop non-import code
    - Otherwise it'll reduce two blank lines down to one, which is
      not only incorrect, but then conflicts with `black`.

--- src/nark/tests/conftest.py
+++ src/nark/tests/conftest.py
@@ -30,6 +30,10 @@ from nark.config import decorate_config
 # Register the fact_factory, etc.
 from nark.tests.item_factories import *  # noqa: F401, F403
 
+# isort: off / Otherwise isort removes one of the following blank lines,
+# but it doesn't if you remove the comment atop the following decorator.
+
+
 # This fixture is used by ``test_helpers`` and ``test_storage``.
 @pytest.fixture
 def endless_fact(fact_factory):
