90c8e72727b78387573470cf810b6efc84e74c7e
Landon Bouma <landonb@users.noreply.github.com>
2023-12-18 13:12:48 -0600

    Linting: Bugfix: Relocate comment so `isort` doesn't contradict `black`
    - For some reason, when the comment lines appears immediately before
      the class definition, isort reduces the number of blank lines after
      the import statement from two blank lines to one blank line.
    
      - I gurgled a bit but couldn't find anything relevant.
    
        On latest `isort`, 5.13.2.
    
        If you really want to be a hero, fix and patch `isort`,
        otherwise just live with this commit.

--- tests/backends/sqlalchemy/test_storage.py
+++ tests/backends/sqlalchemy/test_storage.py
@@ -27,11 +27,12 @@ from nark.config import decorate_config
 # but it doesn't if you remove the comment atop the following class.
 
 
-# The reason we see a great deal of count == 0 statements is to make sure that
-# db rollback works as expected. Once we are confident in our sqlalchemy/pytest
-# setup those are not really needed.
 class TestStore(object):
     """Tests to make sure our store/test setup behaves as expected."""
+
+    # The reason we see a great deal of count == 0 statements is to make sure that
+    # db rollback works as expected. Once we are confident in our sqlalchemy/pytest
+    # setup those are not really needed.
 
     def test_build_is_not_persistent(self, alchemy_store, alchemy_category_factory):
         """
